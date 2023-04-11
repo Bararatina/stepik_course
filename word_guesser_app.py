6fc9c077dde5a0d2f44d786b60de8d47efc16020
Bararatina <lee9494@mail.ru>
2023-04-11 21:19:23 +0700

    Creating hangman app
    При отгадывании букв, он все буквы заменяет на последнюю введенную и получается дичь
    я понял, что дело в letters и что каждый новый цикл я ее ввожу заново и,
    соответственно, все предыдущие значения letters принимают наше последнее значение.
    На строке 130 мой первый вариант, я его закомментил и попробовал через списки сделать,
    но выходит то же самое.
    Вопрос: что нужно поменять, чтобы letters не применял последнее значение к предыдущим циклам?

--- word_guesser_app.py
+++ word_guesser_app.py
@@ -15,11 +15,10 @@ word_list = ['Кант', 'Хроника', 'Зал', 'Галера', 'Балл',
              'Кордебалет', 'Заварушка']
 
 
-def get_word():
-    return random.choice(word_list).upper()
-
-
-print(get_word())
+# def get_word():
+#     word = random.choice(word_list).upper()
+#     return word
+word = random.choice(word_list).upper()
 
 
 # функция получения текущего состояния
@@ -41,7 +40,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|/
                    |      |
-                   |     / 
+                   |     /
                    -
                 ''',
                 # голова, торс, обе руки
@@ -51,7 +50,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|/
                    |      |
-                   |      
+                   |
                    -
                 ''',
                 # голова, торс и одна рука
@@ -61,7 +60,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|
                    |      |
-                   |     
+                   |
                    -
                 ''',
                 # голова и торс
@@ -71,7 +70,7 @@ def display_hangman(tries):
                    |      O
                    |      |
                    |      |
-                   |     
+                   |
                    -
                 ''',
                 # голова
@@ -79,20 +78,67 @@ def display_hangman(tries):
                    --------
                    |      |
                    |      O
-                   |    
-                   |      
-                   |     
+                   |
+                   |
+                   |
                    -
                 ''',
                 # начальное состояние
                 '''
                    --------
                    |      |
-                   |      
-                   |    
-                   |      
-                   |     
+                   |
+                   |
+                   |
+                   |
                    -
                 '''
     ]
     return stages[tries]
+
+
+def play(word):
+    list_word_comp = []
+    list_num_letters = []
+    word_completion = '_' * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    list_word_comp.extend(word_completion)
+    print('Давайте играть в угадайку слов!')
+    print(display_hangman(6))
+    while tries != 0:
+        print(*list_word_comp)
+        print('Введите букву или слово целиком')
+        letter = input().upper()
+        if len(letter) == 1:
+            while not (1040 <= ord(letter) <= 1071 or ord(letter) == 1105) or letter in guessed_letters:
+                print('Некорректный ввод или вы вводили эту букву. Буква Ё = Е Попробуйте еще раз.')
+                letter = input().upper()
+            else:
+                guessed_letters.append(letter)
+                if letter in word:
+                    a = word.find(letter)
+                    while a != -1:
+                        word = word.replace(letter, '_', 1)
+                        list_num_letters.append(a)
+                        a = word.find(letter)
+                    for i in range(len(list_num_letters)):
+                        del list_word_comp[list_num_letters[i]]
+                        list_word_comp.insert(list_num_letters[i], letter)
+                        # word_completion = word_completion[:list_num_letters[i]] + letter + word_completion[(list_num_letters[i] + 1):]
+                elif letter not in word:
+                    print('Такой буквы нет')
+                    tries -= 1
+                    print(display_hangman(tries))
+        if '_' not in list_word_comp:
+            print('Поздравляем! Вы угадали слово!')
+            print(*list_word_comp)
+            break
+    else:
+        print('Вы проиграли!')
+
+
+print(word)
+play(word)
